f0a9538238a7b2adf2306b50cef0ffbf1b5890f0
Frederic R <frederic.rousseau.fr@gmail.com>
2020-04-10 22:13:00 +0100

    Create app.js

---
+++ apps/tandemtales/app.js
@@ -0,0 +1,7 @@
+g.clear();
+g.setFontAlign(0,0); // center font
+g.setFont("Vector",20); // vector font, 80px  
+// draw the current counter value
+g.drawString("Tandem Tales",120,120);
+// optional - this keeps the watch LCD lit up
+g.flip();
